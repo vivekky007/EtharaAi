f3b60384f599e1b7db36b25de46747851e7acf69
vivekky007 <143671272+vivekky007@users.noreply.github.com>
2026-05-08 18:07:20 +0530

    Add role-based access middleware

---
+++ Backend/middleware/role.js
@@ -0,0 +1,12 @@
+module.exports = (...roles) => {
+  return (req, res, next) => {
+
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        msg: "Access denied",
+      });
+    }
+
+    next();
+  };
+};
